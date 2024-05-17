cc27e1f67b078936755044e492c57e5f32884c2a
caro <cirier.c@gmail.com>
2024-05-17 10:47:45 +0200

    ajout étapes recette lasagnes

--- index.php
+++ index.php
@@ -2,20 +2,24 @@
 
 $recipes = [
     [
-        'title' => 'Cassoulet',
-        'recipe' => 'Etape 1 : des flageolets !',
+        'title' => 'Lasagnes végétariennes',
+        'recipe' => 'Etape 1 - Coupez et faire cuire les champignons, puis réserver.<br>
+        Etape 2 - Pelez et hachez l’oignon et l’ail. Faites revenir à la poele. </br>
+        Etape 3 - Ajoutez le beurre et la farine dans la pele. Ensuite incorporer petit à petit le lait en remuant.</br>
+        Etape 4 - Dans un plat allant au four, superposez les feuilles de lasagne, le mélange oignons-ail / béchamel, champignons, épinards, tomates séchées et parmesan.</br>
+        Etape 5 - Enfournez 30 min à 200°C. ',
         'author' => 'mickael.andrieu@exemple.com',
         'is_enabled' => true,
     ],
     [
-        'title' => 'Couscous',
-        'recipe' => 'Etape 1 : de la semoule',
+        'title' => 'Chili sin carne',
+        'recipe' => 'Etape 1 : coupez les carottes et les faire cuire à la poele',
         'author' => 'mickael.andrieu@exemple.com',
         'is_enabled' => false,
     ],
     [
-        'title' => 'Escalope milanaise',
-        'recipe' => 'Etape 1 : prenez une belle escalope',
+        'title' => 'Burger végé',
+        'recipe' => 'Etape 1 : coupez lles champignons et faites les cuire à la poele, réserver.',
         'author' => 'mathieu.nebra@exemple.com',
         'is_enabled' => true,
     ],
